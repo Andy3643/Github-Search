4ec8b3ccffe2a577dd4a90ad04d604ccabec3717
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-04-18 17:13:12 +0300

    feat:add api to github user services.

--- src/app/services/github-info.service.ts
+++ src/app/services/github-info.service.ts
@@ -1,44 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Userinfo } from '../userinfo';
+import { Observable } from 'rxjs';
+import { ApiResponse } from "../models/api_response.model";
+// import { ACCESS_KEY } from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProfileServiceService {
-  private username: string;
-  private clientId: 'Iv1.e706d4a58d05c638';
-  private clientSecret: '63e5962d7b7135b86560f341d7d22422d532f9cb';
-  Userinfo: any;
+export class HttpService {
 
-  constructor(private http: HttpClient) {
-    this.username = 'andy3643';
-  }
-  getProfileInfo(): Observable<Userinfo[]> {
-    return this.http
-      .get<Userinfo[]>(
-        'https://api.github.com/users/' +
-          this.username +
-          '?client_id=' +
-          this.clientId +
-          '&client_secret= ' +
-          this.clientSecret
-      )
-      .pipe(map((res) => res));
-  }
-  getProfileRepos(): Observable<Repository[]> {
-    return this.http
-      .get<Repository[]>(
-        'https://api.github.com/users/' +
-          this.username +
-          '/repos?client_id=' +
-          this.clientId +
-          '&client_secret= ' +
-          this.clientSecret
-      )
-      .pipe(map((res) => res));
-  }
+  constructor(private _http:HttpClient) { }
 
-  updateProfile(username: string) {
-    this.username = username;
+  request(user:string, location:string,arg:string="") :Observable<any>{
+    return this._http.get<ApiResponse>(`https://api.github.com/${location}/${user}${arg}`, {
+    })
+  }
+  search(search:string="") :Observable<any>{
+    return this._http.get<ApiResponse>(`https://api.github.com/users/${search}/repos`, )
   }
 }
