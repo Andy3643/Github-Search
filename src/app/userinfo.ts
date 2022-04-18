f1e9a2ce442af503ec001e26752f43d8c79d73e0
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-04-18 14:40:57 +0300

    file:add userinfo class to define the useer info neede from github

---
+++ src/app/userinfo.ts
@@ -0,0 +1,11 @@
+export class Userinfo {
+    name: string;
+      login: string;
+      avatar_url: string;
+      bio: string;
+      followers: number;
+      following: number;
+      //public_repos: number;
+      email: string;
+      html_url: string;
+}
