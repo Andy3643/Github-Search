b59059339bfadb810d9dd463066075b041d02def
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-04-18 14:52:19 +0300

    feat:add fucntion to collect user input and search in gitnub API

--- src/app/services/github-info.service.ts
+++ src/app/services/github-info.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Userinfo } from '../userinfo';
 
 @Injectable({
   providedIn: 'root'
 })
-export class GithubInfoService {
+export class ProfileServiceService {
+  private username: string;
+  private clientId: 'Iv1.e706d4a58d05c638';
+  private clientSecret: '63e5962d7b7135b86560f341d7d22422d532f9cb';
+  Userinfo: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.username = 'andy3643';
+  }
+  getProfileInfo(): Observable<Userinfo[]> {
+    return this.http
+      .get<Userinfo[]>(
+        'https://api.github.com/users/' +
+          this.username +
+          '?client_id=' +
+          this.clientId +
+          '&client_secret= ' +
+          this.clientSecret
+      )
+      .pipe(map((res) => res));
+  }
+
+  updateProfile(username: string) {
+    this.username = username;
+  }
 }
