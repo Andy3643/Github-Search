aaa1e68e25e2c393e8dbb7a07fa2e6b78e78fd43
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-04-19 02:50:40 +0300

    fix:correct import class location

--- src/app/services/github-info.service.ts
+++ src/app/services/github-info.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ApiResponse } from "../models/api_response.model";
+import { ApiFeedback } from "../interfaces/api-feedback";
 // import { ACCESS_KEY } from "../../environments/environment";
 
 @Injectable({
@@ -12,10 +12,10 @@ export class HttpService {
   constructor(private _http:HttpClient) { }
 
   request(user:string, location:string,arg:string="") :Observable<any>{
-    return this._http.get<ApiResponse>(`https://api.github.com/${location}/${user}${arg}`, {
+    return this._http.get<ApiFeedback>(`https://api.github.com/${location}/${user}${arg}`, {
     })
   }
   search(search:string="") :Observable<any>{
-    return this._http.get<ApiResponse>(`https://api.github.com/users/${search}/repos`, )
+    return this._http.get<ApiFeedback>(`https://api.github.com/users/${search}/repos`, )
   }
 }
